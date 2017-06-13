95ab7842a5663d4476bb6d8d4add2cadc1a83a35
Anton Ovechkin <owanturist@gmail.com>
2017-06-13 15:57:21 +0700

    Eslint errors was fixed in Bitwise.js

--- src/Native/Bitwise.js
+++ src/Native/Bitwise.js
@@ -1,13 +1,12 @@
-var _elm_lang$core$Native_Bitwise = function() {
-
+// eslint-disable-next-line camelcase, brace-style
+var _elm_lang$core$Native_Bitwise = (function _elm_lang$core$Native_Bitwise() {
 return {
 	and: F2(function and(a, b) { return a & b; }),
 	or: F2(function or(a, b) { return a | b; }),
 	xor: F2(function xor(a, b) { return a ^ b; }),
 	complement: function complement(a) { return ~a; },
-	shiftLeftBy: F2(function(offset, a) { return a << offset; }),
-	shiftRightBy: F2(function(offset, a) { return a >> offset; }),
-	shiftRightZfBy: F2(function(offset, a) { return a >>> offset; })
+	shiftLeftBy: F2(function shiftLeftBy(offset, a) { return a << offset; }),
+	shiftRightBy: F2(function shiftRightBy(offset, a) { return a >> offset; }),
+	shiftRightZfBy: F2(function shiftRightZfBy(offset, a) { return a >>> offset; })
 };
-
-}();
+})();
