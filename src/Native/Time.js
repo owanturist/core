9f7ba1402c97a96dd9dfe175afadf979b68b3574
Anton Ovechkin <owanturist@gmail.com>
2017-06-13 18:41:31 +0700

    Eslint errors was fixed in Time

--- src/Native/Time.js
+++ src/Native/Time.js
@@ -1,21 +1,24 @@
-//import Native.Scheduler //
+/* global
+	_elm_lang$core$Native_Scheduler
+*/
 
-var _elm_lang$core$Native_Time = function() {
-
-var now = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
+// eslint-disable-next-line camelcase, brace-style
+var _elm_lang$core$Native_Time = (function _elm_lang$core$Native_Time() {
+var now = _elm_lang$core$Native_Scheduler.nativeBinding(function job(callback)
 {
 	callback(_elm_lang$core$Native_Scheduler.succeed(Date.now()));
 });
 
 function setInterval_(interval, task)
 {
-	return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)
+	return _elm_lang$core$Native_Scheduler.nativeBinding(function job()
 	{
-		var id = setInterval(function() {
+		var id = setInterval(function intervalCb()
+		{
 			_elm_lang$core$Native_Scheduler.rawSpawn(task);
 		}, interval);
 
-		return function() { clearInterval(id); };
+		return function clearInterval_() { clearInterval(id); };
 	});
 }
 
@@ -23,5 +26,4 @@ return {
 	now: now,
 	setInterval_: F2(setInterval_)
 };
-
-}();
+})();
