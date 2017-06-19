21ef2150f54d06a0a0bcb2bb678896770bd2475e
Anton Ovechkin <owanturist@gmail.com>
2017-06-19 14:44:36 +0700

    Eslint errors in Native/Char.js was fixed

--- src/Native/Char.js
+++ src/Native/Char.js
@@ -1,14 +1,24 @@
-//import Native.Utils //
+/* global
+	_elm_lang$core$Native_Utils
+*/
 
-var _elm_lang$core$Native_Char = function() {
-
+// eslint-disable-next-line camelcase, brace-style
+var _elm_lang$core$Native_Char = (function _elm_lang$core$Native_Char() {
 return {
-	fromCode: function(c) { return _elm_lang$core$Native_Utils.chr(String.fromCharCode(c)); },
-	toCode: function(c) { return c.charCodeAt(0); },
-	toUpper: function(c) { return _elm_lang$core$Native_Utils.chr(c.toUpperCase()); },
-	toLower: function(c) { return _elm_lang$core$Native_Utils.chr(c.toLowerCase()); },
-	toLocaleUpper: function(c) { return _elm_lang$core$Native_Utils.chr(c.toLocaleUpperCase()); },
-	toLocaleLower: function(c) { return _elm_lang$core$Native_Utils.chr(c.toLocaleLowerCase()); }
+	fromCode: function fromCode(c)
+	{
+		return _elm_lang$core$Native_Utils.chr(String.fromCharCode(c));
+	},
+	toCode: function toCode(c) { return c.charCodeAt(0); },
+	toUpper: function toUpper(c) { return _elm_lang$core$Native_Utils.chr(c.toUpperCase()); },
+	toLower: function toLower(c) { return _elm_lang$core$Native_Utils.chr(c.toLowerCase()); },
+	toLocaleUpper: function toLocaleUpper(c)
+	{
+		return _elm_lang$core$Native_Utils.chr(c.toLocaleUpperCase());
+	},
+	toLocaleLower: function toLocaleLower(c)
+	{
+		return _elm_lang$core$Native_Utils.chr(c.toLocaleLowerCase());
+	}
 };
-
-}();
+})();
