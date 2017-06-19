d51a298d66100e8c69108233dc9716eba9e24ba7
Anton Ovechkin <owanturist@gmail.com>
2017-06-19 14:45:07 +0700

    Eslint errors in Native/Date.js was fixed

--- src/Native/Date.js
+++ src/Native/Date.js
@@ -1,12 +1,17 @@
-//import Result //
+/* global
+	_elm_lang$core$Result$Err
+	_elm_lang$core$Result$Ok
+*/
 
-var _elm_lang$core$Native_Date = function() {
-
+// eslint-disable-next-line camelcase, brace-style
+var _elm_lang$core$Native_Date = (function _elm_lang$core$Native_Date() {
 function fromString(str)
 {
 	var date = new Date(str);
 	return isNaN(date.getTime())
-		? _elm_lang$core$Result$Err('Unable to parse \'' + str + '\' as a date. Dates must be in the ISO 8601 format.')
+		? _elm_lang$core$Result$Err(
+			'Unable to parse \'' + str + '\' as a date. Dates must be in the ISO 8601 format.'
+		)
 		: _elm_lang$core$Result$Ok(date);
 }
 
@@ -18,16 +23,15 @@ var monthTable =
 
 return {
 	fromString: fromString,
-	year: function(d) { return d.getFullYear(); },
-	month: function(d) { return { ctor: monthTable[d.getMonth()] }; },
-	day: function(d) { return d.getDate(); },
-	hour: function(d) { return d.getHours(); },
-	minute: function(d) { return d.getMinutes(); },
-	second: function(d) { return d.getSeconds(); },
-	millisecond: function(d) { return d.getMilliseconds(); },
-	toTime: function(d) { return d.getTime(); },
-	fromTime: function(t) { return new Date(t); },
-	dayOfWeek: function(d) { return { ctor: dayTable[d.getDay()] }; }
+	year: function year(d) { return d.getFullYear(); },
+	month: function month(d) { return { ctor: monthTable[d.getMonth()] }; },
+	day: function day(d) { return d.getDate(); },
+	hour: function hour(d) { return d.getHours(); },
+	minute: function minute(d) { return d.getMinutes(); },
+	second: function second(d) { return d.getSeconds(); },
+	millisecond: function millisecond(d) { return d.getMilliseconds(); },
+	toTime: function toTime(d) { return d.getTime(); },
+	fromTime: function fromTime(t) { return new Date(t); },
+	dayOfWeek: function dayOfWeek(d) { return { ctor: dayTable[d.getDay()] }; }
 };
-
-}();
+})();
